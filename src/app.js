d979cf71bb218d0922df56fa1714d3237263feb2
vikassharma221525@gmail.com <vikassharma221525@gmail.com>
2026-02-06 20:02:57 +0530

    prepare backend for Render

--- src/app.js
+++ src/app.js
@@ -11,7 +11,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+        process.env.FRONTEND_URL,
+        "http://localhost:5173"
+    ],
     credentials: true
 }));
 app.use(express.json());
